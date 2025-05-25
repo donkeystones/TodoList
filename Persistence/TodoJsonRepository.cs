95c26564a06f9b7dca88720f3d062b6effac34ea
donkeystones <oscar.back96@outlook.com>
2025-05-25 21:59:22 +0200

    Added getTodos and addTodo to persistence

--- Persistence/TodoJsonRepository.cs
+++ Persistence/TodoJsonRepository.cs
@@ -1,6 +1,43 @@
+using System.Text.Json;
+using Persistence.Models;
+
 namespace Persistence;
 
 public class TodoJsonRepository
 {
+    private readonly string _filePath;
+
+    public TodoJsonRepository(string filePath)
+    {
+        _filePath = filePath;
+        
+        if(!File.Exists(_filePath))
+            File.WriteAllText(_filePath, "[]"); //Creates file and adds empty array
+    }
+
+    public List<TodoItem> GetTodos()
+    {
+        var json = File.ReadAllText(_filePath);
+        return JsonSerializer.Deserialize<List<TodoItem>>(json, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        }) ?? new List<TodoItem>();
+    }
     
+    public TodoItem AddTodo(TodoItem item)
+    {
+        var todos = GetTodos();
+        todos.Add(item);
+        SaveTodos(todos);
+        return item;
+    }
+    
+    private void SaveTodos(List<TodoItem> todos)
+    {
+        var json = JsonSerializer.Serialize(todos, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+        File.WriteAllText(_filePath, json);
+    }
 }
